922a762394d42d1165aa4fa89233cb8af290a4da
Tabitha4 <tabithalevine@hotmail.co>
2018-07-21 19:51:44 -0400

    modify some css3 and add html5 tags

--- js/work.js
+++ js/work.js
@@ -4,38 +4,43 @@ var works = [
     pic: "img/cropScout.jpg",
     url: "http://5a346222a114771d0057ca2a.coordinator-seams-12282.bitballoon.com/cropscoutsite/scout"
   },
+
   {
     title: "",
     pic: "img/gallery.jpg",
     url: "http://5a346222a114771d0057ca2a.coordinator-seams-12282.bitballoon.com/my%20gallery/gallery"
   },
+
   {
     title: "",
     pic: "img/calc.jpg",
     url: "http://coordinator-seams-12282.bitballoon.com/calculator/calculator"
   },
+
   {
     title: "",
     pic: "img/shiny.jpg",
     url: "https://shiny-objects-source.herokuapp.com/"
   },
 
-
 	{
     title: "",
     pic: "img/mad.jpg",
     url: "http://5a8afbf97b6ee82b44b6b9de.coordinator-seams-12282.bitballoon.com/Madlibs/index"
   },
+
   {
     title: "",
     pic: "img/MLS.jpg",
     url: "http://coordinator-seams-12282.bitballoon.com/Bootstrap4Site/index"
   },
+
   {
     title: "Project 7 Coming Soon",
     pic: "img/plainCoffee1-isaac-benhesed-atUnsplash.jpg",
     url: "#"
   },
+	
   {
     title: "Project 8 Coming Soon",
     pic: "img/plainCoffee1-isaac-benhesed-atUnsplash.jpg",
